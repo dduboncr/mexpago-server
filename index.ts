9d419c3e1b47bfec2d6eb32eb444fde034fd06ff
Diego Dubon <diego@opendatafabric.com>
2022-06-30 21:24:54 -0600

    feat(updating): added routes for success response

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (_req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+app.post('/success', (req: Request, res: Response) => {
+  res.json({ data: req.body });
 });
 
 app.listen(port, () => {
