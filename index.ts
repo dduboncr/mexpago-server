4cca00e084b39b32ed373a5b868c24d3886fd734
Diego Dubon <diego@opendatafabric.com>
2022-06-30 21:10:33 -0600

    feat(updating): init project

--- index.ts
+++ index.ts
@@ -26,10 +26,6 @@ server.post('/error', async (request, reply) => {
   };
 });
 
-server.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('running server');
 });
